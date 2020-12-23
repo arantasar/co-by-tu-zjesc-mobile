f38cc920ea83208671a41000144103d7d3111521
Janusz Guzowski <janusz.guzowski@gmail.com>
2020-12-23 14:48:04 +0100

    Add user panel

---
+++ components/userPanel/GlassLink/GlassLink.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const GlassLink = ({text, to, handler}) => {
+  const nav = useNavigation();
+
+  const clickHandler = () => {
+    console.log('Wywoluje');
+    if (handler) {
+      handler();
+    } else {
+      nav.navigate(to);
+    }
+  };
+
+  return (
+    <Glass onPress={clickHandler}>
+      <Text>{text}</Text>
+    </Glass>
+  );
+};
+
+export default GlassLink;
+
+const Glass = styled(TouchableOpacity)`
+  padding: 20px;
+  margin: 5px 10px;
+  border-radius: 10px;
+  background-color: rgba(255, 255, 255, 0.4);
+`;
--- views/Panel/Panel.js
+++ views/Panel/Panel.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import {ScrollView, View, Text} from 'react-native';
+import React, {useContext} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {ScrollView, Image, View, Text, ImageBackground} from 'react-native';
+import userContext from '../../context/UserContext';
 import Profile from './Profile';
+import background from './../../assets/backgrounds/kola5b.png';
+import styled from 'styled-components';
+import GlassLink from '../../components/userPanel/GlassLink/GlassLink';
 
-const Panel = () => (
-  <ScrollView>
-    <View>
+const Panel = () => {
+  const ctx = useContext(userContext);
+  const navigation = useNavigation();
+  const logoutHandler = () => {
+    ctx.logout();
+    navigation.navigate('home');
+  };
+
+  const links = [
+    {text: 'Moje przepisy', to: 'home'},
+    {text: 'Dodaj przepis', to: 'home'},
+    {text: 'Ulubione', to: 'home'},
+    {text: 'Edytuj', to: 'home'},
+    {text: 'Wyloguj się', handler: logoutHandler},
+  ];
+
+  return (
+    <Content>
       <Profile />
-      <Text>Moje przepisy</Text>
-      <Text>Dodaj przepis</Text>
-      <Text>Ulubione</Text>
-      <Text>Edytuj profil</Text>
-      <Text>Wyloguj</Text>
-    </View>
-  </ScrollView>
-);
+      <ScrollView>
+        <Background source={background}>
+          {links.map(({text, to, handler}) => (
+            <GlassLink key={text} text={text} to={to} handler={handler} />
+          ))}
+        </Background>
+      </ScrollView>
+    </Content>
+  );
+};
 
 export default Panel;
+
+const Background = styled(ImageBackground)`
+  flex: 1;
+  padding: 5px;
+`;
+
+const Content = styled(View)`
+  flex: 1;
+`;
