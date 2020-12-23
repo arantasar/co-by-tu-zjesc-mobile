7b0a68e487a9db84d330e6e704bbaa3927c45974
Janusz Guzowski <janusz.guzowski@gmail.com>
2020-12-23 22:28:49 +0100

    Get user recipes on user login

--- views/UserRecipes/UserRecipes.js
+++ views/UserRecipes/UserRecipes.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {useEffect} from 'react';
 import {Text, View} from 'react-native';
+import {errorHandler} from '../../common/errorHandler';
+import userContext from '../../context/UserContext';
+import axios from './../../axios';
 
-const UserRecipes = () => (
-  <View>
-    <Text>User Recipes</Text>
-  </View>
-);
+const UserRecipes = () => {
+  const {user} = useContext(userContext);
+  const [recipes, setRecipes] = useState([]);
+  const id = user && user.id;
+
+  useEffect(() => {
+    console.log(id);
+    axios
+      .get(`api/users/${id}/recipes`)
+      .then((res) => setRecipes(res.data))
+      .catch(errorHandler);
+  }, []);
+
+  return (
+    <View>
+      <Text>User Recipes</Text>
+    </View>
+  );
+};
 
 export default UserRecipes;
