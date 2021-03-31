a47b66e8e822b955b2277bd786ad4f1448ea8f4c
Janusz Guzowski <janusz.guzowski@gmail.com>
2021-03-31 22:00:16 +0200

    Add favourites

--- views/Favourites/Favourites.js
+++ views/Favourites/Favourites.js
@@ -1,10 +1,31 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {useEffect} from 'react';
+import {ScrollView, View} from 'react-native';
+import {errorHandler} from '../../common/errorHandler';
+import RecipeCard from '../../components/common/RecipeCard/RecipeCard';
+import Background from './../../components/common/Background/Background';
+import userContext from '../../context/UserContext';
+import background from './../../assets/backgrounds/kola8b.png';
+import styled from 'styled-components';
 
-const Favourites = () => (
-  <View>
-    <Text>Favourites</Text>
-  </View>
-);
+const Favourites = () => {
+  const {user} = useContext(userContext);
+
+  return (
+    <StyledView>
+      <Background image={background}>
+        <ScrollView>
+          {user.favourites.map((recipe) => (
+            <RecipeCard recipe={recipe} key={recipe.id} />
+          ))}
+        </ScrollView>
+      </Background>
+    </StyledView>
+  );
+};
 
 export default Favourites;
+
+const StyledView = styled(View)`
+  flex: 1;
+`;
